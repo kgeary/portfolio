806eb0c4360e74f9c9e7680bd09db3f7b20062d7
Kevin Geary <kgeary@gmail.com>
2020-05-04 15:58:46 -0500

    Updates

---
+++ precache-manifest.c02edb3141306e7b075fc3e0be9903e7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aaf1908961653be5649f72fce0bbe98",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2740b6d787967fe232b8",
+    "url": "/portfolio/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "5f868ae22acf17b5f01d",
+    "url": "/portfolio/static/css/main.c5f40307.chunk.css"
+  },
+  {
+    "revision": "2740b6d787967fe232b8",
+    "url": "/portfolio/static/js/2.c6403902.chunk.js"
+  },
+  {
+    "revision": "7ea21cf83fd98f392cea78cd04e6b0fa",
+    "url": "/portfolio/static/js/2.c6403902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f868ae22acf17b5f01d",
+    "url": "/portfolio/static/js/main.6b8b6c03.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "3572fe3283d942ae00679461af4f8cb1",
+    "url": "/portfolio/static/media/atxnow.3572fe32.jpg"
+  },
+  {
+    "revision": "7e73439f789c3b3e7c3ab300f8d4a60b",
+    "url": "/portfolio/static/media/employee-directory.7e73439f.jpg"
+  },
+  {
+    "revision": "f4e18e8e557246d77bb198b4963008f7",
+    "url": "/portfolio/static/media/employee-tracker.f4e18e8e.jpg"
+  },
+  {
+    "revision": "eac979e3e7772993e60c92645d209c90",
+    "url": "/portfolio/static/media/fitness-tracker.eac979e3.jpg"
+  },
+  {
+    "revision": "df7591cfdb40792b837c20c3af22f906",
+    "url": "/portfolio/static/media/handlebars.df7591cf.jpg"
+  },
+  {
+    "revision": "0a38c39aa0ba68374b7e7fed8516b76a",
+    "url": "/portfolio/static/media/kg-wide.0a38c39a.jpg"
+  },
+  {
+    "revision": "37fe6d9d02750fa27000c29facb77911",
+    "url": "/portfolio/static/media/movie-review.37fe6d9d.jpg"
+  },
+  {
+    "revision": "0a4294dccb2d384aa7689d58576249a5",
+    "url": "/portfolio/static/media/roster-rocket.0a4294dc.jpg"
+  },
+  {
+    "revision": "1d954e17647d3898bff627ffbfc24b9a",
+    "url": "/portfolio/static/media/scheduler.1d954e17.jpg"
+  },
+  {
+    "revision": "6e776d67f8d3901da6f1d92d0f08ad0e",
+    "url": "/portfolio/static/media/showRatings.6e776d67.jpg"
+  },
+  {
+    "revision": "fa5e7c51eb7627bec745292e30e580d1",
+    "url": "/portfolio/static/media/template.fa5e7c51.jpg"
+  },
+  {
+    "revision": "79e84eba3a081378da16ba550790c755",
+    "url": "/portfolio/static/media/weather.79e84eba.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://localhost:3000/precache-manifest.4f6f28d0d3b998fbe813ff777c3fd1bd.js"
+  "/portfolio/precache-manifest.c02edb3141306e7b075fc3e0be9903e7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://localhost:3000/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
