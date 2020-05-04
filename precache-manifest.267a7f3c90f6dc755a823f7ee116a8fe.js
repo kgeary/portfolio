444095f64e4e161f1c9f97962be891371aad757d
Kevin Geary <kgeary@gmail.com>
2020-05-04 15:03:25 -0500

    Updates

---
+++ precache-manifest.267a7f3c90f6dc755a823f7ee116a8fe.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe1051c4fc6abbd78de2bf9ae4c71f9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1f6476f22d3ad85fc3a3",
+    "url": "/portfolio/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "8edf4fc205461e3c050e",
+    "url": "/portfolio/static/css/main.c5f40307.chunk.css"
+  },
+  {
+    "revision": "1f6476f22d3ad85fc3a3",
+    "url": "/portfolio/static/js/2.c902e9ea.chunk.js"
+  },
+  {
+    "revision": "7ea21cf83fd98f392cea78cd04e6b0fa",
+    "url": "/portfolio/static/js/2.c902e9ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8edf4fc205461e3c050e",
+    "url": "/portfolio/static/js/main.4f627d08.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "3572fe3283d942ae00679461af4f8cb1",
+    "url": "/portfolio/static/media/atxnow.3572fe32.jpg"
+  },
+  {
+    "revision": "7e73439f789c3b3e7c3ab300f8d4a60b",
+    "url": "/portfolio/static/media/employee-directory.7e73439f.jpg"
+  },
+  {
+    "revision": "f4e18e8e557246d77bb198b4963008f7",
+    "url": "/portfolio/static/media/employee-tracker.f4e18e8e.jpg"
+  },
+  {
+    "revision": "eac979e3e7772993e60c92645d209c90",
+    "url": "/portfolio/static/media/fitness-tracker.eac979e3.jpg"
+  },
+  {
+    "revision": "df7591cfdb40792b837c20c3af22f906",
+    "url": "/portfolio/static/media/handlebars.df7591cf.jpg"
+  },
+  {
+    "revision": "0a38c39aa0ba68374b7e7fed8516b76a",
+    "url": "/portfolio/static/media/kg-wide.0a38c39a.jpg"
+  },
+  {
+    "revision": "37fe6d9d02750fa27000c29facb77911",
+    "url": "/portfolio/static/media/movie-review.37fe6d9d.jpg"
+  },
+  {
+    "revision": "0a4294dccb2d384aa7689d58576249a5",
+    "url": "/portfolio/static/media/roster-rocket.0a4294dc.jpg"
+  },
+  {
+    "revision": "1d954e17647d3898bff627ffbfc24b9a",
+    "url": "/portfolio/static/media/scheduler.1d954e17.jpg"
+  },
+  {
+    "revision": "6e776d67f8d3901da6f1d92d0f08ad0e",
+    "url": "/portfolio/static/media/showRatings.6e776d67.jpg"
+  },
+  {
+    "revision": "fa5e7c51eb7627bec745292e30e580d1",
+    "url": "/portfolio/static/media/template.fa5e7c51.jpg"
+  },
+  {
+    "revision": "79e84eba3a081378da16ba550790c755",
+    "url": "/portfolio/static/media/weather.79e84eba.jpg"
+  }
+]);
