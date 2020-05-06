cd94b5511402ca94d0cd3ed5d2333e2a05370ca9
Kevin Geary <kgeary@gmail.com>
2020-05-06 13:25:17 -0500

    Updates

---
+++ precache-manifest.0a660481198a63c9cdf802dc52f019fa.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf86d5d7c58ff1bbf5c243d45c1ff3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7707b46cfff0fcfbd2e9",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "23767ca3bc75a8acddd3",
+    "url": "/static/css/main.4d89a4ec.chunk.css"
+  },
+  {
+    "revision": "7707b46cfff0fcfbd2e9",
+    "url": "/static/js/2.82c34086.chunk.js"
+  },
+  {
+    "revision": "7ea21cf83fd98f392cea78cd04e6b0fa",
+    "url": "/static/js/2.82c34086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23767ca3bc75a8acddd3",
+    "url": "/static/js/main.77dee249.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "3572fe3283d942ae00679461af4f8cb1",
+    "url": "/static/media/atxnow.3572fe32.jpg"
+  },
+  {
+    "revision": "7e73439f789c3b3e7c3ab300f8d4a60b",
+    "url": "/static/media/employee-directory.7e73439f.jpg"
+  },
+  {
+    "revision": "f4e18e8e557246d77bb198b4963008f7",
+    "url": "/static/media/employee-tracker.f4e18e8e.jpg"
+  },
+  {
+    "revision": "eac979e3e7772993e60c92645d209c90",
+    "url": "/static/media/fitness-tracker.eac979e3.jpg"
+  },
+  {
+    "revision": "0a38c39aa0ba68374b7e7fed8516b76a",
+    "url": "/static/media/kg-wide.0a38c39a.jpg"
+  },
+  {
+    "revision": "37fe6d9d02750fa27000c29facb77911",
+    "url": "/static/media/movie-review.37fe6d9d.jpg"
+  },
+  {
+    "revision": "0a4294dccb2d384aa7689d58576249a5",
+    "url": "/static/media/roster-rocket.0a4294dc.jpg"
+  },
+  {
+    "revision": "6e776d67f8d3901da6f1d92d0f08ad0e",
+    "url": "/static/media/showRatings.6e776d67.jpg"
+  }
+]);
