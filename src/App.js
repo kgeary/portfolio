b7cd2ef08c31261801abaccb60c7949529fb7da6
Kevin Geary <kgeary@gmail.com>
2020-05-06 13:22:33 -0500

    Changed router basename to no longer user subdomain

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <ViewportProvider>
       <div className="App">
-        <Router basename="/portfolio">
+        <Router basename="/">
           <Nav />
           <div className="Content">
             <Switch>
